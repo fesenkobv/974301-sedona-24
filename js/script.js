de39e270a13ca13bd5681f4b99e03f273886de61
fesenkobv <fesenko@mail.ru>
2019-03-18 23:39:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 // var searchHotels = document.querySelector(".search-hotels");
 var formSearch = document.querySelector(".search-form");
 var btnSearch = document.getElementById("search-button-call");
-
 // Если включен JS, то скрываем форму
 if (formSearch) {
-  formSearch.classList.add("js-form-close");
+	formSearch.classList.add("js-form-close");
 }
-
 if (btnSearch) {
-  // Клик произошёл по кнопке поиска гостиницы
-  btnSearch.addEventListener("click", function(event) {
-    formSearch.classList.toggle("js-form-close");
-    formSearch.classList.toggle("js-form-show");
-  });
+	// Клик произошёл по кнопке поиска гостиницы
+	btnSearch.addEventListener("click", function(event) {
+		formSearch.classList.toggle("js-form-close");
+		formSearch.classList.toggle("js-form-show");
+	});
 }
